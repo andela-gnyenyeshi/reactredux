ded1ea1d966d0a210083fd1e049d10dec25b80c7
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2016-09-19 10:09:17 +0300

    [API] Adds app API

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,7 +1,20 @@
- export function createCourse(course) {
+import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
+
+export function createCourse(course) {
    debugger;
    return {
-     type: 'CREATE_COURSE',
+     type: types.CREATE_COURSE,
      course
    };
  }
+
+export function loadCourses() {
+  return dispatch => {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error); 
+    });
+  };
+}
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,34 +1,32 @@
 import React, { PropTypes} from 'react';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    this.state = {
-      course: {
-        title: ''
-      }
-    };
+     
 
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
+    // this.onTitleChange = this.onTitleChange.bind(this);
+    // this.onClickSave = this.onClickSave.bind(this);
   }
 
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course });
-  }
+  // onTitleChange(event) {
+  //   const course = this.state.course;
+  //   course.title = event.target.value;
+  //   this.setState({course: course });
+  // }
 
-  onClickSave() {
-
-    // console.log(this.props.courses);
-    // this.props.dispatch(courseActions.createCourse(this.state.course));
-    this.props.createCourse(this.state.course);
-    // alert(`Saving ${this.state.course.title}`);
-  }
+  // onClickSave() {
+  //
+  //   // console.log(this.props.courses);
+  //   // this.props.dispatch(courseActions.createCourse(this.state.course));
+  //   // this.props.createCourse(this.state.course);
+  //   this.props.actions.createCourse(this.state.course);
+  //   // alert(`Saving ${this.state.course.title}`);
+  // }
 
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
@@ -39,16 +37,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title} />
-
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave} />
       </div>
     );
   }
@@ -57,7 +45,8 @@ class CoursesPage extends React.Component {
 CoursesPage.propTypes = {
   // dispatch: PropTypes.func.isRequired,
   courses: PropTypes.array.isRequired,
-  createCourse: PropTypes.func.isRequired
+  actions: PropTypes.object.isRequired
+  // createCourse: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -69,7 +58,8 @@ function mapStateToProps(state, ownProps) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    createCourse: course => dispatch(courseActions.createCourse(course))
+    // createCourse: course => dispatch(courseActions.createCourse(course))
+    actions: bindActionCreators(courseActions, dispatch )
   };
 }
 
