0ac5975eab440506c057a42a34384896521df1cf
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2016-09-21 07:41:29 +0300

    [App Redux] Add author action

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,2 +1,3 @@
 export const LOAD_COURSES_SUCCESS = 'LOAD_COURSES_SUCCESS';
+export const LOAD_AUTHORS_SUCCESS = 'LOAD_AUTHORS_SUCCESS';
 // Could have a LOAD_COURSE_FAILURE to handle the error from promise
